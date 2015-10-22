4a0d32cb5bf538a4585fdf4bfe37f7b5df73d677
Tony W <investtwon89@gmail.com>
2015-10-22 18:40:30 -0400

    Implement work and free buttons and toggle

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
+//= require bootstrap-switch
 //= require turbolinks
 //= require_tree .
