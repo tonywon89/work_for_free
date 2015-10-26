519d0264f385a49665de902fc296d23762d9986f
Tony W <investtwon89@gmail.com>
2015-10-25 22:08:08 -0400

    Add static controller and views, with some refactoring

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Work for Free"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Work for Free"
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Work for Free"
+  end
 end
