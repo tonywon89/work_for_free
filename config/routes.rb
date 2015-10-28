3349522af377d3d627bdf50f25721c585b1af702
Tony W <investtwon89@gmail.com>
2015-10-28 08:55:32 -0400

    Filled in layout and added navbar login and sign up

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#home'
 
-  get 'static_pages/help'
+  get 'help' => 'static_pages#help'
 
-  get 'static_pages/about'
+  get 'about' => 'static_pages#about'
   #root to: 'application#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
