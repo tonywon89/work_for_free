e8ae6c17244fa0dbd252abcd724a848c198504cb
Tony W <investtwon89@gmail.com>
2015-11-02 16:33:11 -0500

    Update css for show page

--- app/helpers/summaries_helper.rb
+++ app/helpers/summaries_helper.rb
@@ -5,7 +5,8 @@ module SummariesHelper
     if total >= 60
       total_min = total / 60
       total_sec = total - (total_min * 60)
-      return "#{total_min} minutes #{total_sec} seconds"
+
+      return total_min > 1 ?  "#{total_min} minutes #{total_sec} seconds" : "#{total_min} minute #{total_sec} seconds"
     else 
       base_string
     end
