8729fa1841cfe97904342f9980b5213a3388fa51
Tony W <investtwon89@gmail.com>
2015-11-03 16:47:26 -0500

    Tidy up code in javascript

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,122 +1,112 @@
+/* Global variables */
 
-//variables to set the counter
+// Variables for the counter
 var seconds = 0, tenSeconds = 0, minutes = 0, tenMinutes = 0, hours = 0; 
 
-var counterTime, freeTime = gon.freeTime, spentTime = 0; //will change the free time to load to become what was stored in the database
+// Keeps track of the free time stored by the user
+var freeTime = gon.freeTime;
 
-var timer = false
+// Keeps track of the indiviual activity
+var spentTime = 0;
+
+// Keeps track of whether the timer is running or not 
+var timer = false;
+
+// Keeps track of the radio button that is checked when the data is submitted
 var $storedButton;
-//var startTime, stopTime, totalTime;
 
   
 $(document).ready(function(){
- /*
-  // loads the bootstrap-toggle js library, since the pipeline isn't working
-  $.getScript("https://gitcdn.github.io/bootstrap-toggle/2.2.0/js/bootstrap-toggle.min.js", function(){
-    //$('#work-toggle').bootstrapToggle();
-    //$('#free-toggle').bootstrapToggle();
-    $('#work-toggle').bootstrapToggle('disable');
-    $('#free-toggle').bootstrapToggle('disable');
-  });
-*/
-    //starts the initial counter and freetime to display.
-    updateAll();
 
-    //initializes the buttons and interval
-    var startButton = $('#start-button'), stopButton = $('#stop-button'), resetButton = $('#reset-button');
-    var interval;
+  // Starts the initial counter and freetime to display.
+  updateAll();
 
+  // Initializes the buttons and interval
+  var startButton = $('#start-button'), stopButton = $('#stop-button'), resetButton = $('#reset-button');
+  var interval;
 
-    //prevents the stop button from being functional when it is 0
-    disable(stopButton);
-    disable(resetButton);
-    
-    //When this is clicked, the button is disabled, and the time starts.
-    startButton.click(function(){
-      timer = true;
-      interval = setInterval(function() {
-        seconds += 1;
-        if (seconds == 10) {
-          seconds = 0;
-          tenSeconds += 1;
-          if (tenSeconds == 6) {
-            tenSeconds = 0;
-            minutes += 1;
-            if (minutes == 10) {
-              minutes = 0;
-              tenMinutes += 1;
-              if (tenMinutes == 6) {
-                tenMinutes = 0;
-                hours += 1;
-              }
-            } 
+  // Prevents the stop and reset button from being functional when timer is not running
+  disable(stopButton);
+  disable(resetButton);
+  
+  //When this is clicked, the button is disabled, and the time starts.
+  startButton.click(function(){
+  timer = true;
+
+    // Update the counter when the interval executes, and is stored in the variable to be stopped later
+    interval = setInterval(function() {
+      seconds += 1;
+      if (seconds == 10) {
+        seconds = 0;
+        tenSeconds += 1;
+        if (tenSeconds == 6) {
+          tenSeconds = 0;
+          minutes += 1;
+          if (minutes == 10) {
+            minutes = 0;
+            tenMinutes += 1;
+            if (tenMinutes == 6) {
+              tenMinutes = 0;
+              hours += 1;
+            }
           } 
-        }
-
-        // updates the free time depending on whether the work-toggle button is on
-        if ($('input[type=radio]:checked').val() == "work") {
-          freeTime += 1;
-        } else {
-          freeTime -= 1;
-        }
-
-        $storedButton = $('input[type=radio]:checked')
-
-        spentTime += 1;
-
-        // updates the counter and FreeTime in real time
-        updateAll();
-
-        //stops the time when FreeTime reaches 0
-        if (freeTime <= 0) {
-          clearInterval(interval);
-          alert("Out of free time")
-          disable(stopButton);
-          activate(resetButton);
+        } 
       }
-      }, 100);
 
-      //if (!startTime) { startTime = setTime(); }
-      
-      //ensures only stop button is functional when the timer is running  
-      disable(startButton);
-      activate(stopButton);
-      disable(resetButton);
+      // Updates the free time depending on whether work or relax button is selected
+      $('input[type=radio]:checked').val() == "work" ? freeTime += 1 : freeTime -= 1;
 
-    });
+      // Stores the selected radio button for data submission when reset button is clicked
+      $storedButton = $('input[type=radio]:checked')
 
-    
+      spentTime += 1;
 
-    //This shows what happens when the stop button is clicked
+      // updates the counter and FreeTime in real time
+      updateAll();
 
-    stopButton.click(function() {
-
-      //stops time and reactivates and disables buttons
-      clearInterval(interval);
-      disable(stopButton);
-      activate(startButton);
-      activate(resetButton);
-      
-      //stopTime = setTime(); 
-    });
-
-    //resets counter when reset button is clicked and resets buttons
-    resetButton.click(reset);
-
-    // Stops the timer, and saves the data 
-    $(':radio').click(function(){
-      if (timer) {
+      //stops the time when FreeTime reaches 0
+      if (freeTime <= 0) {
         clearInterval(interval);
+        alert("Out of free time")
         disable(stopButton);
         activate(resetButton);
-        reset();
-      }
+    }
+    }, 100);
+    
+    // Ensures only stop button is functional when the timer is running  
+    disable(startButton);
+    activate(stopButton);
+    disable(resetButton);
 
-    });
+  });
+
+  //This shows what happens when the stop button is clicked
+  stopButton.click(function() {
+
+    // Stops the counter and reactivates and disables buttons
+    clearInterval(interval);
+    disable(stopButton);
+    activate(startButton);
+    activate(resetButton);
+  });
+
+  // When the reset button is clicked, resets the counter and saves the data
+  resetButton.click(reset);
+
+  $(':radio').click(function(){
+
+    // Only execute if the timer is running. Otherwise, nothing happens
+    if (timer) {
+      clearInterval(interval);
+      disable(stopButton);
+      activate(resetButton);
+      reset();
+    }
+  });
 });
     
 
-
+/* Defined Functions */
 
 // Disables the button so it cannot be clicked  
 function disable(button) {
@@ -128,59 +118,45 @@ function activate(button) {
   button.prop("disabled", false);
 }
 
-// resets the counter and the buttons to initial conditions
+/* Resets the counter and the buttons to initial conditions. 
+ * Also sends an AJAX request to server to save the free time data and activity data
+ */
 function reset() {
-  //totalTime = counterTime;
-  //startTime = undefined;
 
-  //resets the counter time
+  // Resets the counter time
   seconds = 0, tenSeconds = 0, minutes = 0, tenMinutes = 0, hours = 0;
       
   updateCounter();
   activate($('#start-button'));
   disable($('#reset-button'))
 
-
-  // Check if timer is running, and only execute if it is
+  /* Execute AJAX request only if the timer has been set to true by clicking the start button,
+   * indicating that there is data to store
+   */
   if (timer) {
-    // Determine if button selected is work or not 
-    var workOrRelax;
 
-    // The description of the activity
-    var description;
+    // Set the workOrRelax value to true or false depending if it is work or not
+    var workOrRelax = $storedButton.val() == "work" ? true : false;
 
-    //Set the workOrRelax value to be a boolean instead of a string
-    if ($storedButton.val() == "work") {
-      workOrRelax = true;
-    } else {
-      workOrRelax = false;
-    }
+    // Stores the description of the activity as the class of the stored button
+    var description = $storedButton.attr('class');
 
-    // stores the description as the class of the stored button
-    description = $storedButton.attr('class');
+    // Stores the url that will be used by the AJAX request
+    var url = "/users/" + gon.user["id"] + "?ftime=" + freeTime + 
+      "&is_work=" + workOrRelax + "&description=" + description + "&stime=" + spentTime
 
     // Sends an AJAX request to the users controller, show action in order to save the data as a record 
     var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "/users/" + gon.user["id"] + "?ftime=" + freeTime + 
-    "&is_work=" + workOrRelax + "&description=" + description + "&stime=" + spentTime, true);
+    xhttp.open("GET", url, true);
     xhttp.send();
 
-    // sets the timer to not be running
+    // Sets the timer to not be running, and inidicate that there is no data to be saved
     timer = false;
   }
-
-
-
-  //console.log(gon.workRelaxButtons)
   
+  // Reset the activity time to 0 
   spentTime = 0;
 }
-/*
-function setTime(time) {
-  var dateObj = new Date();
-  return dateObj.toLocaleTimeString();
-}
-*/
 
 // returns a string of the counter time
 function setCounter (h, tm, m, ts, s) {
@@ -189,9 +165,7 @@ function setCounter (h, tm, m, ts, s) {
 
 // updates the counter in real time to display on the webpage
 function updateCounter() {
-  counterTime = setCounter(hours, tenMinutes, minutes, tenSeconds, seconds);
-  $('#counter').text(counterTime);
- 
+  $('#counter').text(setCounter(hours, tenMinutes, minutes, tenSeconds, seconds));
 }
 
 // updates the display of the free time
@@ -202,7 +176,7 @@ function updateFreeTime() {
     var freeMinutes = Math.floor(freeTime/60);
     var freeSeconds = parseInt(freeTime % 60);
 
-    // To display plural minutes or singular
+    // To display plural minutes or singular 
     if (freeMinutes == 1) {
       $('#free').text(freeMinutes + " minute " + freeSeconds + " seconds" );
     }
@@ -220,16 +194,3 @@ function updateAll() {
   updateFreeTime();
 }
 
-
-/*
-// Toggles the buttons and resets the counter. Disables toggle so it cannot be clicked
-function toggle() {
-  $('#free-toggle').bootstrapToggle('enable');
-  $('#free-toggle').bootstrapToggle('toggle');
-  $('#free-toggle').bootstrapToggle('disable');
-  $('#work-toggle').bootstrapToggle('enable');
-  $('#work-toggle').bootstrapToggle('toggle');
-  $('#work-toggle').bootstrapToggle('disable');
-  reset();
-}
-*/
