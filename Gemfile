6d07f8542af7133fa602657eac6949f3b02f93f6
Tony W <investtwon89@gmail.com>
2015-11-02 20:01:18 -0500

    Add basic pie chart for work and relax activities

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'bootstrap-sass', '3.2.0.0'
 gem "bootstrap-switch-rails"
 gem 'devise'
 gem 'gon'
+gem "googlecharts", :require => "gchart"
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .js.coffee assets and views
--- app/controllers/summaries_controller.rb
+++ app/controllers/summaries_controller.rb
@@ -4,9 +4,60 @@ class SummariesController < ApplicationController
     @totalWork = 0
     @totalRelax = 0 
 
+    work_hash = {}
+    work_data = []
+    work_labels = []
+
+    relax_hash = {}
+    relax_data = []
+    relax_labels = []
+
     @records.each do |record|
       time_spent = record.time_spent  
       record.is_work ? @totalWork += time_spent : @totalRelax += time_spent
+
+      if record.is_work 
+        if work_hash[record.description] === nil
+          work_hash[record.description] = time_spent
+        else
+          work_hash[record.description] += time_spent
+        end
+      else 
+        if relax_hash[record.description] === nil
+          relax_hash[record.description] = time_spent
+        else
+          relax_hash[record.description] += time_spent
+        end
+      end
     end
+
+    work_hash.each do |description, time_spent|
+          work_data.push(time_spent)
+          work_labels.push(description)
+    end
+
+    relax_hash.each do |description, time_spent|
+      relax_data.push(time_spent)
+      relax_labels.push(description)
+    end
+
+    @work_graph = Gchart.pie(
+      title: "Summary of Work Activity",
+      data: work_data,
+      labels: work_labels,
+      bar_colors: 'FF0000',
+      title_color: 'FF0000',
+      size: '500x300'
+      )
+
+    @relax_graph = Gchart.pie(
+      title: "Summary of Relax Activity",
+      data: relax_data,
+      labels: relax_labels,
+      bar_colors: '3DA8CC',
+      title_color: 'FF0000',
+      size: '500x300'
+      )
+    
   end
 end
