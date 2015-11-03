9af93657c4d3cef4f66818f4c1851f7931dc4039
Tony W <investtwon89@gmail.com>
2015-11-03 15:38:36 -0500

    Finish displaying pie chart and update summary table and show page format

--- app/helpers/summaries_helper.rb
+++ app/helpers/summaries_helper.rb
@@ -10,7 +10,28 @@ module SummariesHelper
     else 
       base_string
     end
+  end
 
+  def pie_chart(records, title, color)
+    records_hash = {}
+    total_time = 0
+    records.each do |record| 
+      time_spent = record.time_spent
+      records_hash[record.description] ||= 0
+      records_hash[record.description] += time_spent
+      total_time += time_spent
+    end
+
+    data = records_hash.collect { |description, time_spent| time_spent }
+    labels = records_hash.collect { |description, time_spent| "#{description} (#{(100.0 *time_spent/total_time).round}%)"}
+
+    Gchart.pie(
+      title: title,
+      data: data,
+      labels: labels,
+      bar_colors: color,
+      size: '695x380'
+      ) 
   end
 
 end
