52d6d425fec2ee13a52f81828fde557ac463cc0b
Tony W <investtwon89@gmail.com>
2015-11-05 12:03:35 -0500

    Fix some css

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,6 @@ Rails.application.routes.draw do
   get 'home' => 'static_pages#home'
   get 'help' => 'static_pages#help'
 
-  get 'about' => 'static_pages#about'
-
-  
-
   get 'buttons/new'
   get 'buttons/edit'
   post 'buttons/create'
